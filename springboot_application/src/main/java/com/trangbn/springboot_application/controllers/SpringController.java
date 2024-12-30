a969103d587a0c9d73a2bf2d5a6c2ebb6d157581
trangbn <trangbn580@gmail.com>
2024-12-30 15:12:36 +0700

    Update a chore for spring framework

--- springboot_application/src/main/java/com/trangbn/springboot_application/controllers/SpringController.java
+++ springboot_application/src/main/java/com/trangbn/springboot_application/controllers/SpringController.java
@@ -3,12 +3,19 @@ package com.trangbn.springboot_application.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class SpringController {
     @RequestMapping(value={"/hello", "/",""})
-    public String hello(Model model) {
+    public String hello( Model model) {
         model.addAttribute("username", "Trangbn");
         return "index.html";
+    }
+
+    @RequestMapping("/getMessageStatus")
+    @ResponseBody
+    public String getMessageStatus() {
+        return "This is first status";
     }
 }

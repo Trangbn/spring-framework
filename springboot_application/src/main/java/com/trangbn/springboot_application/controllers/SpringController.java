fa6bdcd8826043174631cc9df39b1a0e331f08cd
Robert <trangbn580@gmail.com>
2025-10-20 17:18:30 +0700

    Start reviewing Spring

--- springboot_application/src/main/java/com/trangbn/springboot_application/controllers/SpringController.java
+++ springboot_application/src/main/java/com/trangbn/springboot_application/controllers/SpringController.java
@@ -1,16 +1,14 @@
 package com.trangbn.springboot_application.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class SpringController {
     @RequestMapping(value={"/hello", "/",""})
-    public String hello( Model model) {
-        model.addAttribute("username", "Trangbn");
-        return "index.html";
+    public String hello() {
+        return "templates/index.html";
     }
 
     @RequestMapping("/getMessageStatus")

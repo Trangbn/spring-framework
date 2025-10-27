3a85dbcbeff41223865cc364dae504faef5583f4
Robert <trangbn580@gmail.com>
2025-10-27 10:58:31 +0700

    Adding validation in for Contact

--- springboot_application/src/main/java/com/trangbn/springboot_application/controllers/ContactController.java
+++ springboot_application/src/main/java/com/trangbn/springboot_application/controllers/ContactController.java
@@ -2,9 +2,12 @@ package com.trangbn.springboot_application.controllers;
 
 import com.trangbn.springboot_application.model.Contact;
 import com.trangbn.springboot_application.services.ContactServices;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -23,9 +26,14 @@ public class ContactController {
     }
 
     @RequestMapping(value = "/saveMsg", method = POST)
-    public ModelAndView saveMessage(Contact contact){
+    public String saveMessage(@Valid @ModelAttribute("contact") Contact contact, Errors errors){
+
+        if (errors.hasErrors()) {
+            log.error("Contact form validation failed due to : " + errors.toString());
+            return "contact.html";
+        }
         contactServices.saveMessageDetails(contact);
-        return new ModelAndView("redirect:/contact");
+        return "redirect:/contact";
     }
 
 }

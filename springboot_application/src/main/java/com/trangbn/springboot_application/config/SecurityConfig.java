6a1f86c33722e328efae0ba8fa72d180eb4d93b5
Robert <trangbn580@gmail.com>
2025-10-28 14:04:49 +0700

    Disabled CSRF by default

---
+++ springboot_application/src/main/java/com/trangbn/springboot_application/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.trangbn.springboot_application.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(requests -> requests.requestMatchers("/", "/home").permitAll()
+                        .requestMatchers("/holidays/**").permitAll()
+                        .requestMatchers("/contact").permitAll()
+                        .requestMatchers("/saveMsg").permitAll()
+                        .requestMatchers("/courses").permitAll()
+                        .requestMatchers("/about").permitAll()
+                        .requestMatchers("/assets/**").permitAll()
+                );
+
+        return http.build();
+
+    }
+
+}

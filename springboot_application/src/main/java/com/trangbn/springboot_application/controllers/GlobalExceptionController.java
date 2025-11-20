724fb209bb518c2be21267c3e8b63b4a84bf64cd
Robert <trangbn580@gmail.com>
2025-11-20 13:15:01 +0700

    Adding Exception handler for Rest controller - logic by rest api

--- springboot_application/src/main/java/com/trangbn/springboot_application/controllers/GlobalExceptionController.java
+++ springboot_application/src/main/java/com/trangbn/springboot_application/controllers/GlobalExceptionController.java
@@ -1,6 +1,8 @@
 package com.trangbn.springboot_application.controllers;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
@@ -11,7 +13,8 @@ exceptions across the whole application in one global handling component. It can
 as an interceptor of exceptions thrown by methods annotated with @RequestMapping and similar.
 * */
 @Slf4j
-@ControllerAdvice
+@ControllerAdvice(annotations = Controller.class)
+@Order(2)
 public class GlobalExceptionController {
 
     /*
@@ -21,10 +24,19 @@ public class GlobalExceptionController {
     * */
     @ExceptionHandler(Exception.class)
     public ModelAndView exceptionHandler(Exception exception){
+        String errorMsg = null;
         ModelAndView errorPage = new ModelAndView();
         errorPage.setViewName("error");
-        errorPage.addObject("errormsg", exception.getMessage());
+        if(exception.getMessage()!=null){
+            errorMsg = exception.getMessage();
+        }else if (exception.getCause()!=null){
+            errorMsg = exception.getCause().toString();
+        }else if (exception!=null){
+            errorMsg = exception.toString();
+        }
+        errorPage.addObject("errormsg", errorMsg);
         return errorPage;
     }
+
 
 }

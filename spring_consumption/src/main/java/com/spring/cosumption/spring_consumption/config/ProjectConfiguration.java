3de8f0b18ecf406e39ff9f72ea1d11b474b4705e
Robert <trangbn580@gmail.com>
2025-11-22 00:29:11 +0700

    WebClient as consumer for Rest api

--- spring_consumption/src/main/java/com/spring/cosumption/spring_consumption/config/ProjectConfiguration.java
+++ spring_consumption/src/main/java/com/spring/cosumption/spring_consumption/config/ProjectConfiguration.java
@@ -2,9 +2,13 @@ package com.spring.cosumption.spring_consumption.config;
 
 import feign.RequestInterceptor;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.boot.web.reactive.function.client.WebClientCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunctions;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Base64;
 
@@ -24,6 +28,11 @@ public class ProjectConfiguration {
     public RestTemplate restTemplate(){
         RestTemplateBuilder restTemplateBuilder = new RestTemplateBuilder();
         return restTemplateBuilder.basicAuthentication("trangbn580@gmail.com", "123456789").build();
+    }
+
+    @Bean
+    public WebClient webClient(){
+        return WebClient.builder().filter(ExchangeFilterFunctions.basicAuthentication("trangbn580@gmail.com", "123456789")).build();
     }
 
 }
